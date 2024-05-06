96c9f160aeb6ed9de63f05e3bfaf170037654035
Matt <kanzoconfigz@hotmail.com>
2024-05-05 21:14:25 -0700

    Use proper active background color for breeze dark

--- src/themes.c
+++ src/themes.c
@@ -24,7 +24,9 @@ int UseTheme(JWM *jwm, ColorTheme theme)
             //@define-color theme_unfocused_base_color_breeze #1b1e20;
             //@define-color theme_unfocused_bg_color_breeze #2a2e32;
             //@define-color theme_unfocused_fg_color_breeze #fcfcfc;
-            jwm->global_bg_color_active = "#2a2e32";
+    
+            //jwm->global_bg_color_active = "#2a2e32";
+            jwm->global_bg_color_active = "#31363B";
             jwm->global_bg_color_inactive = "#2a2e32";
 
             jwm->global_fg_color_active = "#fcfcfc";
