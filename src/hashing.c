fb2d178f9238ef94c33c2464d0935f2313d65888
Matt W <kanzoconfigz@hotmail.com>
2024-09-25 21:41:56 -0700

    Use FNV1a 64 bit numbers for arm64 also.

--- src/hashing.c
+++ src/hashing.c
@@ -12,7 +12,7 @@
 // 64 bit and 32 bit fnv1-a
 static size_t Hash(const char *key)
 {
-#ifdef __x86_64__
+#if defined (__x86_64__) || defined (__aarch64__)
     const size_t fnv_prime = 1099511628211;
     size_t hash = 14695981039346656037u; // FNV offset basis
 #else
